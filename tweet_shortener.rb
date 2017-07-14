94c312381eac15f9e72b9ad6cc6a675ea3c8b835
Hui Wang <huiw3939@gmail.com>
2017-07-14 00:27:20 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+# Write your code here.'
+require 'pry'
+def dictionary
+  {"hello" => "hi", 'to' => '2',"two" => '2', 'too' => '2', 'four' => '4', 'for' => '4', 'be' => 'b', 'you' => 'u', 'at' => '@', 'and' => '&'}
+end
+
+def word_substituter (tweet)
+  arr = tweet.split(" ")
+  arr.map do |i|
+    dictionary.map do |x,y|
+      if i.downcase == x
+        i.replace(y)
+    end
+  end
+end
+arr.join(" ")
+end
+
+
+def bulk_tweet_shortener (tweet)
+  tweet.map{|x| puts word_substituter(x)}
+end
+
+def selective_tweet_shortener (tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    return tweet
+end
+end
+
+def shortened_tweet_truncator (tweet)
+  x = word_substituter(tweet)
+  if x.length > 140
+    return x[1..140]
+  else
+    return x
+  end
+end
